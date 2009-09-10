e8773cba46994a6f1c5c28e269a4995ee191fafc
loeffler <loeffler@0ea37897-9c56-0410-909c-f838baec3fdf>
2009-09-10 22:12:32 +0000

    - improved wrapper script - renamed launch configurations - added readme for v1.0.0

--- trunk/jidgen.sh
+++ trunk/jidgen.sh
@@ -3,10 +3,18 @@
 echo "Trying to setup the environment..."
 
 CWD=`dirname $0`
-JIDGEN_JAR=`ls ${CWD}/jidgen-*.jar 2>/dev/null`
-if [ $? != 0 ]; then
-	echo "Jidgen archive not found. Exiting."
-	exit -1
+cd $CWD
+
+JIDGEN_JAR=`find . -maxdepth 0 -name jidgen*.jar|head -n1`
+if [ -z "$JIDGEN_JAR" ]; then
+
+	# put some more effort 
+	JIDGEN_JAR=`find . -name jidgen*.jar|head -n1`
+	if [ -z "$JIDGEN_JAR" ]; then
+
+		echo "Jidgen archive not found. Exiting."
+		exit -1
+	fi
 fi
 echo "Found jidgen archive \"${JIDGEN_JAR}\".";
 
@@ -15,4 +23,4 @@ echo "Set classpath to \"${CP}\"."
 
 echo
 echo "Starting jidgen..."
-java -cp $CP de.rrze.idmone.utils.jidgen.IdGenerator $*
+java -cp $CP de.rrze.idmone.utils.jidgen.IdGenerator $@
